1f32a84adb008f422663543c09fe1e2eb938fddc
Виктор <shark_vik@inbox.ru>
2016-10-09 12:17:29 +0300

    fix

--- framework/packagesLoader.php
+++ framework/packagesLoader.php
@@ -12,7 +12,7 @@ function RegisterNamespaces( $rootPath )
 		{
 			while( ( $file = readdir( $dh ) ) !== false )
 			{
-				if( $file != "." && $file != ".." && strpos($file, 'www') !== false )
+				if( $file != "." && $file != ".." )
 				{
 					$fileWithRoot = $fullPath."/".$file;
 					if( is_dir( $fileWithRoot ) )
